33f6ed74a5d3c12288cdd169f83bc9e6217b8953
N Magee <nem2p@virginia.edu>
2026-03-24 13:48:12 -0400

    Cleanup

--- papermill.sh
+++ papermill.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-papermill earthquakes.ipynb output.ipynb
+# papermill earthquakes.ipynb output.ipynb
 
 # papermill earthquakes.ipynb /dev/null
 
-# papermill earthquakes.ipynb output.ipynb -p starttime '2025-03-01' -p endtime '2025-06-31'
+papermill earthquakes.ipynb output.ipynb -p starttime '2025-03-01' -p endtime '2025-06-31'
 
